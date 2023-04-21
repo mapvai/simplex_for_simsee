52de4e576924c71f2f48f8334bed693e1d6077ba
Marco perez <mapflick@gmail.com>
2023-04-21 14:50:40 -0300

    simsee_simplex.h actualizado con las variables que se usan en GPU para ejecutar el algoritmo por etapas

--- simplex_simsee.h
+++ simplex_simsee.h
@@ -10,12 +10,14 @@ struct TSimplexGPUs {
     int* top; // largo NVariables
     int8_t* flg_y;
     int* left; // largo NRestricciones
-    int* lstvents;
-	char* lstAcoplesVEnts;
     double* mat; // filas = NRestricciones + 1 (por la funcion a maximizar
                          // columnas = NVariables + 1 (por el termino independiente)
 };
 
 typedef TSimplexGPUs* TDAOfSimplexGPUs;
 
+struct TSimplexVars {
+	int res, cnt_igualdades, cnt_columnasFijadas, cnt_RestrInfactibles, cnt_variablesLiberadas;
+};
 
+typedef TSimplexVars* TDAOfSimplexVars;
